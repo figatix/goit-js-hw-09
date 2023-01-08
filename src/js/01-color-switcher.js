4998b70abd5ff6dbdb41f5bb9b0e5a941a82af0c
figatix <60873339+figatix@users.noreply.github.com>
2023-01-08 21:24:49 +0200

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const DELAY = 1000;
 
+const startEl = document.querySelector('button[data-start]')
+const stopEl = document.querySelector('button[data-stop]')
+const bodyEl = document.querySelector('body')
+
+let intervalId = null;
+
+function onStartClick(e) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  intervalId = setInterval((() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }), DELAY)
+
+  e.target.disabled = true;
+  stopEl.disabled = false
+}
+
+function onStopClick(e) {
+
+  clearInterval(intervalId)
+
+  e.target.disabled = true
+  startEl.disabled = false
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startEl.addEventListener('click', onStartClick)
+stopEl.addEventListener('click', onStopClick)
